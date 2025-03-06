998ee7916fe8f83910abdb6506be7f081707afae
Jacob Stewart <jacob.g.stewart@icloud.com>
2025-03-06 14:37:17 -0500

    add cors middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-from app.db.session import SessionDep
-from app.models.user import UserDB
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routers import auth
 
-from app.utils.auth import generate_password_hash
+origins = ["http://localhost:5173"]
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
