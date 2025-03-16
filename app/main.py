4e34e2cc4338ac144b94fa5350d94490872425c5
Jacob Stewart <jacob.g.stewart@icloud.com>
2025-03-16 16:34:16 -0400

    setup local https

--- app/main.py
+++ app/main.py
@@ -3,17 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import auth
 
-origins = ["http://localhost:5173"]
-
 
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["https://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(auth.router)
---
+++ start-dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+uvicorn app.main:app \
+  --host 0.0.0.0 \
+  --port 8000 \
+  --reload \
+  --ssl-keyfile localhost-key.pem \
+  --ssl-certfile localhost.pem
